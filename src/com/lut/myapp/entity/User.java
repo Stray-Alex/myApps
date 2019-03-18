2f7cea249ebe8870a189d63f4409b820e6bda6d3
Stray-Alex <1005218593@qq.com>
2019-03-18 23:50:44 +0800

    my first commit in eclipse

---
+++ src/com/lut/myapp/entity/User.java
@@ -0,0 +1,13 @@
+package com.lut.myapp.entity;
+
+/********************************
+ * @类名 User.java 
+ * @作者 alex
+ * @日期 2019年3月18日-下午11:12:23
+ * @版本 
+ * @描述 
+ *********************************/
+public class User {
+	String username;
+
+}
